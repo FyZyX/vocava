6e4a99375bccd6527e2aead89f195f1b0d2fce67
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-26 13:47:19 -0700

    add database to migration configuration

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -1,9 +1,10 @@
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from alembic import context
+from vocava import model
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,8 +18,8 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = model.Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
