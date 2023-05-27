95d41037c53dc0c1bcbe0284d8357edcfef13b1a
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-26 22:28:20 -0700

    create basic streamlit chatbot

--- docs/DATABASE.md
+++
@@ -1,8 +0,0 @@
-## Database Migrations
-
-1. Install Alembic: `pip install alembic`
-2. Navigate to your project root directory and initiate Alembic: `alembic init alembic`
-3. Configure Alembic: Open the `alembic.ini` file that was generated in your project root directory and change the `sqlalchemy.url` to the URI of your database.
-4. Modify `env.py`: You'll need to provide the `Base` from your SQLAlchemy models to Alembic's `env.py` file in the `alembic` directory.
-5. Generate a migration: After you've made changes to your models, you can generate a migration script with `alembic revision --autogenerate -m "description of your changes"`. This will create a new script in the `alembic/versions` directory.
-6. Apply migrations: You can upgrade your database to a later version with `alembic upgrade head` or downgrade to an earlier version with `alembic downgrade -1`.
--- src/vocava/main.py
+++ src/vocava/main.py
@@ -1,5 +1,53 @@
+import requests
+import streamlit as st
+from streamlit_chat import message
+
+
+def query(payload):
+    API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
+    headers = {"Authorization": st.secrets['api_key']}
+
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+
+def get_text():
+    return st.text_input("You: ", "Hello, how are you?", key="input")
+
+
 def main():
-    pass
+    st.set_page_config(
+        page_title="Streamlit Chat - Demo",
+        page_icon=":robot:"
+    )
+
+    st.header("Vocava - Demo")
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
+
+    user_input = get_text()
+
+    if user_input:
+        output = query({
+            "inputs": {
+                "past_user_inputs": st.session_state.past,
+                "generated_responses": st.session_state.generated,
+                "text": user_input,
+            }, "parameters": {"repetition_penalty": 1.33},
+        })
+
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(output["generated_text"])
+
+    if st.session_state['generated']:
+
+        for i in range(len(st.session_state['generated']) - 1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
 
 if __name__ == '__main__':
