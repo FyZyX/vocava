32bd3651510dbec14d7fbf0b371ab081e6d2d953
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-26 22:44:56 -0700

    use anthropic model for completion

--- src/vocava/main.py
+++ src/vocava/main.py
@@ -1,14 +1,21 @@
-import requests
+import os
+
+import anthropic
 import streamlit as st
 from streamlit_chat import message
 
+ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
 
-def query(payload):
-    API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
-    headers = {"Authorization": st.secrets['api_key']}
 
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
+def query(prompt):
+    client = anthropic.Client(api_key=ANTHROPIC_API_KEY)
+    response = client.completion(
+        prompt=f"{anthropic.HUMAN_PROMPT} {prompt}{anthropic.AI_PROMPT}",
+        stop_sequences=[anthropic.HUMAN_PROMPT],
+        model="claude-v1.3-100k",
+        max_tokens_to_sample=100,
+    )
+    return response["completion"]
 
 
 def get_text():
@@ -31,20 +38,17 @@ def main():
 
     user_input = get_text()
 
+    context = ""
     if user_input:
-        output = query({
-            "inputs": {
-                "past_user_inputs": st.session_state.past,
-                "generated_responses": st.session_state.generated,
-                "text": user_input,
-            }, "parameters": {"repetition_penalty": 1.33},
-        })
-
+        current_inp = f"{anthropic.HUMAN_PROMPT} {user_input}{anthropic.AI_PROMPT}"
         st.session_state.past.append(user_input)
-        st.session_state.generated.append(output["generated_text"])
+        context += current_inp
+
+        completion = query(prompt=context)
+        st.session_state.generated.append(completion)
+        context += completion
 
     if st.session_state['generated']:
-
         for i in range(len(st.session_state['generated']) - 1, -1, -1):
             message(st.session_state["generated"][i], key=str(i))
             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
