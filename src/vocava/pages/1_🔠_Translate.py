0da9c9726f3e410efbf6519bcb24be492a465447
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-28 18:11:16 -0700

    add debug checkbox to translator app

--- src/vocava/pages/1_🔠_Translate.py
+++ src/vocava/pages/1_🔠_Translate.py
@@ -9,15 +9,18 @@ ANTHROPIC_API_KEY = st.secrets["anthropic_api_key"]
 def main():
     st.title('Translation')
 
+    if st.checkbox("DEBUG Mode", value=True):
+        model = mock.MockLanguageModel()
+    else:
+        model = anthropic.Claude(ANTHROPIC_API_KEY)
+    translator = Translator(model)
+
     col1, col2 = st.columns(2)
     from_lang = col1.text_input("From Language", "en")
     to_lang = col2.text_input("To Language", "fr")
 
     text_to_translate = st.text_area("Enter text to translate")
     if st.button("Translate"):
-        model = anthropic.Claude(ANTHROPIC_API_KEY)
-        model = mock.MockLanguageModel()
-        translator = Translator(model)
         with st.spinner():
             translated_text = translator.translate(text_to_translate, from_lang, to_lang)
         st.divider()
