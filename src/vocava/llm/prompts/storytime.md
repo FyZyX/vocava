25587116489974cb6c921bd3014ce55be39d4f09
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-05-29 03:42:23 -0700

    get a translated version of the story as well

---
+++ src/vocava/llm/prompts/storytime.md
@@ -0,0 +1,18 @@
+Create a short, engaging story suitable for a language learner at a fluency level of ${fluency} / 10.
+Fluency of 1 / 10 should be story a child could understand. Fluency of 10 / 10 should contain sophisticated language at nearly native capability.
+The story should be in ${target_language} and revolve around the theme: "${concept}".
+Additionally, generate a list of at least three reading comprehension questions and associated answers.
+Finally, translate all of this into ${native_language} for the learner's convenience.
+
+Respond with a parsable JSON payload as if you were a REST API. Remember to escape any quotes inside a JSON string.
+Use the following output format:
+{
+  "story": {
+    "$target_language": "<story>",
+    "$native_language": "<translated-story>"
+  },
+  "questions": [
+    {"question_$target_language": "<question-1>", "answer_$target_language": "<answer-1>", "question_$native_language": "<translated-question-1>", "answer_$native_language": "<translated-answer-1>"},
+    ...
+  ]   
+}
--- src/vocava/pages/2_📚_Storytime.py
+++ src/vocava/pages/2_📚_Storytime.py
@@ -14,17 +14,20 @@ class Storyteller:
     def __init__(self, model: LanguageModel):
         self._model = model
 
-    def generate_story(self, language: str, fluency: int,
-                       concept: str) -> dict[str, Any]:
+    def generate_story(self, native_language: str, target_language: str,
+                       fluency: int, concept: str) -> dict[str, Any]:
         prompt = load_prompt(
-            "storytime-generate",
-            language=language,
+            "storytime",
+            native_language=native_language,
+            target_language=target_language,
             fluency=fluency,
             concept=concept,
         )
-        response = self._model.generate(prompt, max_tokens=1_000)
+        response = self._model.generate(prompt, max_tokens=2_000)
         try:
-            return json.loads(response)
+            start = response.find("{")
+            payload = response[start:]
+            return json.loads(payload)
         except json.decoder.JSONDecodeError:
             st.write(response)
 
@@ -40,30 +43,42 @@ def main():
     else:
         model = anthropic.Claude(ANTHROPIC_API_KEY)
 
-    language = st.sidebar.selectbox("Choose Language", options=LANGUAGES, index=12)
+    native_language = st.sidebar.selectbox("Native Language", options=LANGUAGES)
+    target_language = st.sidebar.selectbox("Choose Language", options=LANGUAGES,
+                                           index=12)
     fluency = st.sidebar.slider("Fluency", min_value=1, max_value=10, step=1)
+    show_native = st.sidebar.checkbox("View in native language")
 
     user_prompt = st.text_input("What kind of story would you like?")
+    native_language_name = LANGUAGES[native_language]["name"]
+    target_language_name = LANGUAGES[target_language]["name"]
     if st.button("Generate Story"):
         storyteller = Storyteller(model)
         with st.spinner():
-            data = storyteller.generate_story(language, fluency, user_prompt)
+            data = storyteller.generate_story(
+                native_language_name,
+                target_language_name,
+                fluency,
+                user_prompt,
+            )
         st.session_state["data"] = data
 
-    if not st.session_state["data"]:
+    language = native_language_name if show_native else target_language_name
+    data = st.session_state["data"]
+    if not data or language not in data["story"]:
         return
 
-    generated_story = st.session_state["data"]["story"]
+    generated_story = st.session_state["data"]["story"][language]
     comprehension_questions = st.session_state["data"]["questions"]
     st.markdown(generated_story)
     for i, item in enumerate(comprehension_questions):
         cols = st.columns(2)
         with cols[0]:
-            st.write(item["question"])
+            st.write(item[f"question_{language}"])
         with cols[1]:
             show_answer = st.checkbox("Show Answer", key=f"q{i}")
         if show_answer:
-            st.success(item["answer"])
+            st.success(item[f"answer_{language}"])
 
 
 if __name__ == "__main__":
