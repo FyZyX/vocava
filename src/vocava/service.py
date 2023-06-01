cf912336ae676d9fce36222a919acfd004525165
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-06-01 15:52:40 -0700

    remove translate module

--- src/vocava/service.py
+++ src/vocava/service.py
@@ -2,6 +2,26 @@ import llm
 import llm.prompt
 
 JSON = None | bool | int | float | str | dict[str, "JSON"] | list["JSON"]
+LANGUAGES = {
+    "🇺🇸 English": {"name": "English", "flag": "🇺🇸", "code": "en"},
+    "🇩🇪 German": {"name": "German", "flag": "🇩🇪", "code": "de"},
+    "🇵🇱 Polish": {"name": "Polish", "flag": "🇵🇱", "code": "pl"},
+    "🇪🇸 Spanish": {"name": "Spanish", "flag": "🇪🇸", "code": "es"},
+    "🇮🇹 Italian": {"name": "Italian", "flag": "🇮🇹", "code": "it"},
+    "🇫🇷 French": {"name": "French", "flag": "🇫🇷", "code": "fr"},
+    "🇵🇹 Portuguese": {"name": "Portuguese", "flag": "🇵🇹", "code": "pt"},
+    "🇮🇳 Hindi": {"name": "Hindi", "flag": "🇮🇳", "code": "hi"},
+    "🇸🇦 Arabic": {"name": "Arabic", "flag": "🇸🇦", "code": "ar"},
+    "🇨🇳 Chinese": {"name": "Chinese", "flag": "🇨🇳", "code": "zh"},
+    "🇬🇷 Greek": {"name": "Greek", "flag": "🇬🇷", "code": "el"},
+    "🇮🇱 Hebrew": {"name": "Hebrew", "flag": "🇮🇱", "code": "he"},
+    "🇯🇵 Japanese": {"name": "Japanese", "flag": "🇯🇵", "code": "ja"},
+    "🇰🇷 Korean": {"name": "Korean", "flag": "🇰🇷", "code": "ko"},
+    "🇷🇺 Russian": {"name": "Russian", "flag": "🇷🇺", "code": "ru"},
+    "🇸🇪 Swedish": {"name": "Swedish", "flag": "🇸🇪", "code": "sv"},
+    "🇵🇭 Tagalog": {"name": "Tagalog", "flag": "🇵🇭", "code": "tl"},
+    "🇻🇳 Vietnamese": {"name": "Vietnamese", "flag": "🇻🇳", "code": "vi"},
+}
 
 
 class Service:
@@ -12,8 +32,8 @@ class Service:
     def run(self, native_language: str, target_language: str, **kwargs) -> JSON:
         prompt = llm.prompt.load_prompt(
             self._name,
-            native_language=native_language,
-            target_language=target_language,
+            native_language=LANGUAGES[native_language]["name"],
+            target_language=LANGUAGES[target_language]["name"],
             **kwargs
         )
         response = self._model.generate(prompt)
