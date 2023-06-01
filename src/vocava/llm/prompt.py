fcb0b3c39044b00539ead36b82cf56facaa490d4
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-06-01 15:28:41 -0700

    add JSON extraction method to prompt module

--- src/vocava/llm/prompt.py
+++ src/vocava/llm/prompt.py
@@ -1,3 +1,4 @@
+import json
 import pathlib
 import string
 
@@ -8,3 +9,13 @@ def load_prompt(name: str, **kwargs):
     with open(PROMPTS_PATH / f"{name}.md", "r") as file:
         prompt_template = string.Template(file.read())
     return prompt_template.substitute(**kwargs)
+
+
+def extract_json(content):
+    content = content.replace("```json", "").replace("```", "")
+    start = content.find("{")
+    payload = content[start:]
+    try:
+        return json.loads(payload)
+    except json.decoder.JSONDecodeError as e:
+        raise ValueError("Invalid JSON returned by model") from e
