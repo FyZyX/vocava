57691beb0de40b4c4f4509f8cfc4ad71069365ea
Lucas Lofaro <lucasmlofaro@gmail.com>
2023-06-02 21:43:30 -0700

    improve style

--- src/vocava/pages/3_🛝_Playground.py
+++ src/vocava/pages/3_🛝_Playground.py
@@ -54,22 +54,22 @@ def vocabulary_practice(user: entity.User, tutor: entity.Tutor):
     word = word_item[user.target_language_name()]
 
     st.divider()
-    _, col, _ = st.columns(3)
+    _, col, _ = st.columns([1, 3, 1])
     with col:
         st.header(f"***:blue[{word}]***")
     st.divider()
     cols = st.columns([1, 2, 3, 2, 2])
     with cols[1]:
-        if st.button("Previous"):
+        if st.button("< Previous"):
             prev_index = max(0, current_index - 1)
             st.session_state["current_index"] = prev_index
             st.experimental_rerun()
     with cols[2]:
         show_answer = st.button("Show Translation", key=current_index)
     with cols[3]:
-        save_word = st.button("Save Word")
+        save_word = st.button("Save")
     with cols[4]:
-        if st.button("Next"):
+        if st.button("Next >"):
             next_index = min(len(vocabulary) - 1, current_index + 1)
             st.session_state["current_index"] = next_index
             st.experimental_rerun()
